804e779731249eb2fda2926af02bcb21665b9ef2
nzbcor <2639365465@qq.com>
2020-03-09 17:37:34 +0800

    An slightly adjust to bakdata.sh
    Caaaaaaaaaaaaaaaaaake I did this to prevent that when the it fails to bak or destination foder is empty or whatever.

--- bakdata.sh
+++ bakdata.sh
@@ -1,9 +1,40 @@
-#！/bin/sh
-echo ”开始执行自动备份==============================================“
-mkdir /backup/mc
-cp -r /root/bds/worlds /backup/mc
-tar -zcPvf /backup/backup$(date +%Y%m%d).tar /backup/mc
-rm -rf /backup/mc/
-echo "开始删除过于陈旧的备份========================================"
-find ./ -mtime +7 -name "*.tar" -exec rm -rf {} \;
-echo "已删除过于陈旧的备份 所有备份工作已完成======================="
+#!/bin/bash
+
+ZIP="/usr/bin/zip"
+RM="/bin/rm"
+
+SOURCE_PATH="/root/bds/worlds"
+DESTINATION_FILE="/backup/mc/MinecraftSaveBackup_`date \"+%s\"`.zip"
+
+SOURCE_PATH_FILES="$SOURCE_PATH/*"
+
+
+if [ -f $DESTINATION_FILE ];then
+    echo "The file $DESTINATION_FILE EXIST! Terminate."
+    exit -1
+fi
+
+if [ "`ls -A $SOURCE_PATH`" = "" ]; then
+    echo "The path $SOURCE_PATH is empty! Terminate."
+    exit 0
+fi
+
+cd $SOURCE_PATH && $ZIP -r $DESTINATION_FILE *
+RET=$?
+
+if [ $RET -ne 0 ];then
+    echo "zip Error. Abort!"
+    exit -2
+fi
+
+cd -
+
+
+echo "Cleanning $SOURCE_PATH_FILES"
+$RM -rf $SOURCE_PATH_FILES
+
+find `dirname $DESTINATION_FILE` -type f -mtime +30 -exec rm -f {} \;
+echo "Done."
+
+# idk what i've done i just write a script for some reasons then boom
+# sry bruh i took down your codes :p
